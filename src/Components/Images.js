68a95e74dc584a028181b367fec5ebf444d6de2b
Onur Eredo <onureredo@yahoo.com>
2023-01-06 10:54:04 +0100

    edits

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -7,7 +7,7 @@ function Images(props){
     const [currentItems, setCurrentItems] = useState([]);
     const [pageCount, setPageCount] = useState(0);
     const [itemOffset, setItemOffset] = useState(0);
-    const itemsPerPage = 6;
+    const itemsPerPage = 9;
 
     useEffect(() => {
         const endOffset = itemOffset + itemsPerPage;
