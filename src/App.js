ff17a4ab9d69503297d2911526584b6c7f3de92b
Onur Eredo <onureredo@yahoo.com>
2023-01-06 10:27:03 +0100

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
+//https://jsonplaceholder.typicode.com/
 function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+    .then(response => response.json() //convert response into JSON
+    .then(data => {
+      setImages(data);
+    }))
+  }, [])
+
+
   return (
     <div className="App">
-      font-family check
+      
     </div>
   );
 }
---
+++ src/Components/Images.js
@@ -0,0 +1,47 @@
+import { useState, useEffect} from 'react';
+import ReactPaginate from 'react-paginate';
+
+//npm package: https://www.npmjs.com/package/react-paginate
+function Images(props){
+    const { data } = props;
+    const [currentItems, setCurrentItems] = useState(null);
+    const [pageCount, setPageCount] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPAge = 6;
+
+    useEffect(() => {
+        const endOffset = itemOffset + itemsPerPAge;
+        setCurrentItems(data.slice(itemOffset, endOffset));
+        setPageCount(Math.ceil(data.length / itemsPerPAge));
+    }, [itemOffset, itemsPerPAge, data]);
+
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % data.length;
+        setItemOffset(newOffset);
+    };
+
+    return (
+        <>
+        <div className='images'>
+            {currentItems.map(image => {
+                return (
+                    <div className='image'>
+                        <img src={image.url} alt={image.title}/>
+                    </div>
+                )
+            })}
+        </div>
+        <ReactPaginate
+            breakLabel= "..."
+            nextLabel= "next >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={5}
+            pageCount={pageCount}
+            previousLabel= "<previous"
+            renderOnZeroPageCount={null}
+        />
+        </>
+    )
+}
+
+export default Images;
